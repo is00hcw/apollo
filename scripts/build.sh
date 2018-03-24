adc62e871b9221ced654217b69c98dcc2c133bf2
is00hcw <is00hcw@163.com>
2018-03-24 16:40:00 +0800

    update mvn

--- scripts/build.sh
+++ scripts/build.sh
@@ -27,19 +27,19 @@ cd ..
 # package config-service and admin-service
 echo "==== starting to build config-service and admin-service ===="
 
-mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password
+/opt/apache-maven-3.3.9/bin/mvn clean package -DskipTests -pl apollo-configservice,apollo-adminservice -am -Dapollo_profile=github -Dspring_datasource_url=$apollo_config_db_url -Dspring_datasource_username=$apollo_config_db_username -Dspring_datasource_password=$apollo_config_db_password
 
 echo "==== building config-service and admin-service finished ===="
 
 echo "==== starting to build portal ===="
 
-mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password $META_SERVERS_OPTS
+/opt/apache-maven-3.3.9/bin/mvn clean package -DskipTests -pl apollo-portal -am -Dapollo_profile=github,auth -Dspring_datasource_url=$apollo_portal_db_url -Dspring_datasource_username=$apollo_portal_db_username -Dspring_datasource_password=$apollo_portal_db_password $META_SERVERS_OPTS
 
 echo "==== building portal finished ===="
 
 echo "==== starting to build client ===="
 
-mvn clean install -DskipTests -pl apollo-client -am $META_SERVERS_OPTS
+/opt/apache-maven-3.3.9/bin/mvn clean install -DskipTests -pl apollo-client -am $META_SERVERS_OPTS
 
 echo "==== building client finished ===="
 
